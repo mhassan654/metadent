e87e92e250416796623cf4bef852dca0cf8ea8eb
User <user@Users-iMac.local>
2023-02-20 17:14:22 +0300

    Update composer

---
+++ Modules/BillingModule/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('billingmodule::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('billingmodule.name') !!}
+    </p>
+@endsection
