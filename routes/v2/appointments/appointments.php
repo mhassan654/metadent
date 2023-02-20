9bf09ce923bd42b682ea34293fb911d7c91698f9
MUWONGE HASSAN <hassansaava@gmail.com>
2023-02-20 08:54:50 +0300

    remove route names

--- routes/v2/appointments/appointments.php
+++ routes/v2/appointments/appointments.php
@@ -9,97 +9,97 @@ Route::group(['prefix' => 'appointments'], function () {
   // All the appointment route
   // Route::group(['middleware' => ['permission:View Appointments']], function () {
 
-  Route::post('all', [AppointmentsController::class, 'index'])->name('appointments.all');
+  Route::post('all', [AppointmentsController::class, 'index']);
 
-  Route::post('appointments-today', [AppointmentsController::class, 'current_day_appointments'])->name('appointments.today');
+  Route::post('appointments-today', [AppointmentsController::class, 'current_day_appointments']);
 
   // Specific appointment record route
-  Route::post('appointment', [AppointmentsController::class, 'show'])->name('appointments.show');
+  Route::post('appointment', [AppointmentsController::class, 'show']);
 
   // doctor appointments
-  Route::post('doctor_appointments', [AppointmentsController::class, 'single_doctor_appointments'])->name('appointments.doctor_appointments');
+  Route::post('doctor_appointments', [AppointmentsController::class, 'single_doctor_appointments']);
 
 
   // Create new appointment record route
-  Route::post('create', [AppointmentsController::class, 'create_appointment'])->name('appointments.create');
+  Route::post('create', [AppointmentsController::class, 'create_appointment']);
 
   // Edit appointment details route
-  Route::post('update', [AppointmentsController::class, 'update_with_recurrence'])->name('appointments.update');
+  Route::post('update', [AppointmentsController::class, 'update_with_recurrence']);
 
-  Route::post('rescheduling', [AppointmentsController::class, 'rescheduling'])->name('appointments.rescheduling');
-
-  // Edit appointment status route
-  Route::post('update/status', [AppointmentsController::class, 'updateStatus'])->name('appointments.change_status');
+  Route::post('rescheduling', [AppointmentsController::class, 'rescheduling']);
 
   // Edit appointment status route
-  Route::post('extend', [AppointmentsController::class, 'extend_appointment'])->name('appointments.extend');
+  Route::post('update/status', [AppointmentsController::class, 'updateStatus']);
+
+  // Edit appointment status route
+  Route::post('extend', [AppointmentsController::class, 'extend_appointment']);
 
   // All the appointment route
-  Route::post('status/all', [AppointmentsController::class, 'getStatus'])->name('appointments.status_list');
+  Route::post('status/all', [AppointmentsController::class, 'getStatus']);
 
-  Route::post('confirm', [AppointmentsController::class, 'confirm_appointment'])->name('appointments.confirm');
+  Route::post('confirm', [AppointmentsController::class, 'confirm_appointment']);
 
-  Route::post('cancel', [AppointmentsController::class, 'cancel_appointment'])->name('appointments.cancel');
+  Route::post('cancel', [AppointmentsController::class, 'cancel_appointment']);
 
-  Route::post('close', [AppointmentsController::class, 'close_appointment'])->name('appointments.close');
+  Route::post('close', [AppointmentsController::class, 'close_appointment']);
 
-  Route::post('waitingroom', [AppointmentsController::class, 'waiting_room'])->name('appointments.waitingroom');
+  Route::post('waitingroom', [AppointmentsController::class, 'waiting_room']);
 
-  Route::post('doctor/waitingroom', [AppointmentsController::class, 'waiting_room_doctor'])->name('appointments.doctor_waitingroom');
+  Route::post('doctor/waitingroom', [AppointmentsController::class, 'waiting_room_doctor']);
 
-  Route::post('servingroom', [AppointmentsController::class, 'serving_room'])->name('appointments.servingroom');
+  Route::post('servingroom', [AppointmentsController::class, 'serving_room']);
 
-  Route::post('closed', [AppointmentsController::class, 'closed_appointments'])->name('appointments.closed');
+  Route::post('closed', [AppointmentsController::class, 'closed_appointments']);
 
-  Route::post('delete', [AppointmentsController::class, 'destroy'])->name('appointments.delete');
+  Route::post('delete', [AppointmentsController::class, 'destroy']);
 
-  Route::post('doctor/serve-patient', [AppointmentsController::class, 'serving_time'])->name('appointments.doctor_serve_patient');
+  Route::post('doctor/serve-patient', [AppointmentsController::class, 'serving_time']);
 
-  Route::post('completed-appointments-today', [AppointmentsController::class, 'completed_appointments_today'])->name('appointments.completed_appointment_today');
+  Route::post('completed-appointments-today', [AppointmentsController::class, 'completed_appointments_today']);
 
 
-  Route::post('update-with-recurrency', [AppointmentsController::class, 'update_with_recurrence'])->name('appointments.update_with_recurrency');
+  Route::post('update-with-recurrency', [AppointmentsController::class, 'update_with_recurrence']);
 
-  Route::post('patient-appointments', [AppointmentsController::class, 'patient_appointments'])->name('appointments.patient');
+  Route::post('patient-appointments', [AppointmentsController::class, 'patient_appointments']);
 
-  Route::post('cancel-recurring-appointments', [AppointmentsController::class, 'cancel_recurring_appointments'])->name('appointments.cancel_recurring');
+  Route::post('cancel-recurring-appointments', [AppointmentsController::class, 'cancel_recurring_appointments']);
 
-  Route::post('archive-recurring-appointments', [AppointmentsController::class, 'archive_recurring_appointments'])->name('appointments.archive_recurring');
+  Route::post('archive-recurring-appointments', [AppointmentsController::class, 'archive_recurring_appointments']);
 
-  Route::post('restore-appointments', [AppointmentsController::class, 'restore_appointments'])->name('appointments.restore');
+  Route::post('restore-appointments', [AppointmentsController::class, 'restore_appointments']);
 
   //waiting patient list
-  Route::post('waiting-patient-list', [AppointmentsController::class, 'waiting_patient_list'])->name('appointments.waiting_patient_list');
+  Route::post('waiting-patient-list', [AppointmentsController::class, 'waiting_patient_list']);
 
   //latest serving appointment
-  Route::post('latest-serving-front-office-appointment', [AppointmentsController::class, 'front_office_department_appointment'])->name('appointments.latest_serving_front_office');
+  Route::post('latest-serving-front-office-appointment', [AppointmentsController::class, 'front_office_department_appointment']);
 
-  Route::post('appointments-counter', [AppointmentsController::class, 'appointments_counter'])->name('appointments.counter');
+  Route::post('appointments-counter', [AppointmentsController::class, 'appointments_counter']);
 
-  Route::post('update-overdue-waiting-to missed', [AppointmentsController::class, 'update_waiting_appointments_to_missed'])->name('appointments.update_waiting_to_missed');
+  Route::post('update-overdue-waiting-to missed', [AppointmentsController::class, 'update_waiting_appointments_to_missed']);
 
-  Route::post('paginated-appointments', [AppointmentsController::class, 'paginated_appointments'])->name('appointments.paginated');
+  Route::post('paginated-appointments', [AppointmentsController::class, 'paginated_appointments']);
 
-  Route::post('dashboard-upcoming-appointments', [AppointmentsController::class, 'dashboard_upcoming_appointments'])->name('appointments.dashboard_upcoming');
+  Route::post('dashboard-upcoming-appointments', [AppointmentsController::class, 'dashboard_upcoming_appointments']);
 
-  Route::post('number-today-appointments', [AppointmentsController::class, 'number_current_day_appointments'])->name('appointments.today_total');
+  Route::post('number-today-appointments', [AppointmentsController::class, 'number_current_day_appointments']);
 
-  Route::get('download-pdf', [AppointmentsController::class, 'download_pdf'])->name('appointments.download_pdf');
+  Route::get('download-pdf', [AppointmentsController::class, 'download_pdf']);
 
-  Route::get('download-excel', [AppointmentsController::class, 'download_excel'])->name('appointments.download_excel');
+  Route::get('download-excel', [AppointmentsController::class, 'download_excel']);
 
-  Route::get('download-csv', [AppointmentsController::class, 'download_csv'])->name('appointments.download_csv');
+  Route::get('download-csv', [AppointmentsController::class, 'download_csv']);
 
-  Route::post('upcoming-appointments', [AppointmentsController::class, 'upcoming_appointments'])->name('appointments.upcoming');
+  Route::post('upcoming-appointments', [AppointmentsController::class, 'upcoming_appointments']);
 
-  Route::post('past-appointments', [AppointmentsController::class, 'past_appointments'])->name('appointments.past');
-  Route::get('recall-appointments', [AppointmentsController::class, 'recallAppointments'])->name('appointments.recalls');
+  Route::post('past-appointments', [AppointmentsController::class, 'past_appointments']);
+  Route::get('recall-appointments', [AppointmentsController::class, 'recallAppointments']);
 
 
-  Route::get('download-patient-appointments-pdf', [AppointmentsController::class, 'download_patient_appointments_pdf'])->name('appointments.download_pdf');
+  Route::get('download-patient-appointments-pdf', [AppointmentsController::class, 'download_patient_appointments_pdf']);
 
-  Route::get('download-patient-appointments-csv', [AppointmentsController::class, 'download_patient_appointments_csv'])->name('appointments.download_csv');
+  Route::get('download-patient-appointments-csv', [AppointmentsController::class, 'download_patient_appointments_csv']);
 
-  Route::get('download-patient-appointments-excel', [AppointmentsController::class, 'download_patient_appointments_excel'])->name('appointments.download_excel');
+  Route::get('download-patient-appointments-excel', [AppointmentsController::class, 'download_patient_appointments_excel']);
 
 });
