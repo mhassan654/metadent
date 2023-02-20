9ca1b2ee1fee0cf354d805a44d73d3dec8958551
User <user@Users-iMac.local>
2023-02-20 17:37:59 +0300

    Remove unsubmoduled modules

--- Modules/HumanResourceModule/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('humanresourcemodule::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('humanresourcemodule.name') !!}
-    </p>
-@endsection
